446a895c1ee96f52d7948b72d6e3f886fd46c9ff
Tadeo <74320188+tadeosoresi@users.noreply.github.com>
2020-12-22 16:59:37 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,4 +3,12 @@ $(function(){
     $('.carouselHNAS').carousel({
         interval: 2200
     });
+    /* Trabajamos con los ID de los li del menu superior */
+    let identificadores = ['#conteoIngreso', '#conteoContacto', '#conteoRegistro'];
+    identificadores.forEach(function(id){
+        $(document).on('show.bs.modal', id,  function(e){
+            console.log('Se ha ingresado a ' + id)
+        });
+    });
   });
+
