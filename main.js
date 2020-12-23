4d067ed6708cac7ebc473bd6b565f0baf7684694
Tadeo Soresi <tadeosoresi23@outlook.com>
2020-12-23 01:09:26 -0300

    agregamos animaciones con JS

--- main.js
+++ main.js
@@ -6,9 +6,20 @@ $(function(){
     /* Trabajamos con los ID de los li del menu superior */
     let identificadores = ['#conteoIngreso', '#conteoContacto', '#conteoRegistro'];
     identificadores.forEach(function(id){
-        $(document).on('show.bs.modal', id,  function(e){
-            console.log('Se ha ingresado a ' + id)
+        $(id).on('click', function(e){
+            console.log('Se ha ingresado a modal ' + id)
         });
     });
+    let identificadores2 = ['#modalContacto', '#modalIngreso'];
+    identificadores2.forEach(function(id2){
+        $(id2).on('click', function(e){
+            console.log('Se ha emitido la peticion de ' + id2)
+        })
+    })
+    const menuSup = document.querySelector('.menuSup');
+    menuSup.style.setProperty('--animate-duration', '3.5s');
+
+    const tituloPrincipal = document.querySelector('.titulo');
+    tituloPrincipal.style.setProperty('--animate-duration', '2.5s');
   });
 
